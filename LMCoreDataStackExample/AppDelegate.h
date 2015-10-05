d306feab48c8dad17bc90f348241b8fcf48c6d24
Lukasz Margielewski <lukasz.margielewski@gmail.com>
2015-10-05 20:43:25 +0200

    Initial Commit

---
+++ LMCoreDataStackExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMCoreDataStackExample
+//
+//  Created by Lukasz Margielewski on 05/10/15.
+//  Copyright © 2015 Lukasz Margielewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMCoreDataStackExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMCoreDataStackExample
+//
+//  Created by Lukasz Margielewski on 05/10/15.
+//  Copyright © 2015 Lukasz Margielewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
